5d99025a58233fa5686997a7deb8b57eaa68f98a
jianminlu <jianminlu@tencent.com>
2017-06-09 19:34:06 +0800

    6.9 signup

--- routes/signin.js
+++ routes/signin.js
@@ -1,16 +1,38 @@
+var sha1 =require('sha1');
 var express = require('express');
 var router = express.Router();
 
-var checkLogin = require('../middlewares/check').checkLogin;
+var UserModel = require('../models/users')
+var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
-// GET 登录页
-router.get('/', checkLogin, function(req, res, next){
-    res.send(req.flash());
+// GET /signin 登录页
+router.get('/', checkNotLogin, function(req, res, next) {
+  res.render('signin');
 });
 
-// POST 用户登录
-router.post('/', checkLogin, function(req, res, next){
-    res.send(req.flash());
+// POST /signin 用户登录
+router.post('/', checkNotLogin, function(req, res, next) {
+  var name = req.fields.name;
+  var password = req.fields.password;
+
+  UserModel.getUserByName(name)
+    .then(function(user){
+      if(!user){
+        req.flash('error', '用户不存在');
+        return res.redirect('back');
+      }
+      // 检查密码是否匹配
+      if(sha1(password) !== user.password){
+        req.flash('error', '用户名或密码错误');
+        return res.redirect('back');
+      }
+      req.flash('success', '登录成功');
+      // 用户信息写入 session
+      delete user.password;
+      req.session.user = user;
+      res.redirect('/posts');
+    })
+    .catch(next);
 });
 
 module.exports = router;
